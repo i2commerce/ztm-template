8cdf724d9d3945c127c2fed2afa1d6906582e7c3
kenneth <itwosdnbhd@gmail.com>
2022-04-09 19:35:06 +0800

    Updated homepage with new title and slogan.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Welcome to i2 Ecommerce!</h1>
+      <p class="mt-5">We help SMEs to digitalize & focus building businesses!</p>
     </div>
   </div>
 </template>
